a368c3e3f2befb44deb88e78861e60f1bd763362
WakiyamaP <wakiyamap@gmail.com>
2017-09-22 00:55:37 +0900

    Fix x86 binary for nis

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -68,7 +68,11 @@ wine "C:/python36/scripts/pyinstaller.exe" --noconfirm --ascii --name $NAME_ROOT
 
 # build NSIS installer
 # $VERSION could be passed to the electrum.nsi script, but this would require some rewriting in the script iself.
-wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" /DPRODUCT_VERSION=$VERSION electrum.nsi
+if [ -d "$WINEPREFIX/drive_c/Program Files (x86)" ]; then
+    wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" /DPRODUCT_VERSION=$VERSION electrum.nsi
+else
+    wine "$WINEPREFIX/drive_c/Program Files/NSIS/makensis.exe" /DPRODUCT_VERSION=$VERSION electrum.nsi
+fi
 
 cd dist
 mv electrum-mona-setup.exe $NAME_ROOT-$VERSION-setup.exe
