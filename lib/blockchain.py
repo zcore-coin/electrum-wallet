31cc3dd7fd16e611fb1b56bd17622fca585bcc5f
WakiyamaP <wakiyamap@gmail.com>
2017-11-02 01:18:12 +0900

    revert ltc-scrypt

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -40,9 +40,10 @@ except ImportError as e:
 
 try:
     import scrypt
-    scrypt.scrypt_1024_1_1_80 = lambda x: scrypt.hash(x, x, N=1024, r=1, p=1, buflen=32)
+    scryptGetHash = lambda x: scrypt.hash(x, x, N=1024, r=1, p=1, buflen=32)
 except ImportError:
-    from . import scrypt
+    util.print_msg("Warning: package scrypt not available, using fallback")
+    from .scrypt import scrypt_1024_1_1_80 as scryptGetHash
 
 def serialize_header(res):
     s = int_to_hex(res.get('version'), 4) \
@@ -161,7 +162,7 @@ class Blockchain(util.PrintError):
         prev_hash = hash_header(prev_header)
         height = header.get('block_height')
         if height < 450000 :
-            _powhash = rev_hex(bh2u(scrypt.scrypt_1024_1_1_80(bfh(serialize_header(header)))))
+            _powhash = rev_hex(bh2u(scryptGetHash(bfh(serialize_header(header)))))
         else:
             _powhash = rev_hex(bh2u(lyra2re2_hash.getPoWHash(bfh(serialize_header(header)))))
         if prev_hash != header.get('prev_block_hash'):
