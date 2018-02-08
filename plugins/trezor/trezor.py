bc5b233f604da6c7785bdff4c003f184c779bd37
脇山P <wakiyamap@gmail.com>
2018-02-08 18:21:57 +0900

    Fix coin_name

--- plugins/trezor/trezor.py
+++ plugins/trezor/trezor.py
@@ -159,7 +159,7 @@ class TrezorPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if NetworkConstants.TESTNET else "Bitcoin"
+        return "Testnet" if NetworkConstants.TESTNET else "Monacoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
