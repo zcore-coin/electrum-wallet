fb06521ae54720b4753c37e15b24f5261efe0bfc
wakiyamap <wakiyamap@gmail.com>
2018-08-06 06:17:30 +0900

    remove print

--- electrum/blockchain.py
+++ electrum/blockchain.py
@@ -396,8 +396,6 @@ class Blockchain(util.PrintError):
 
 
     def get_target(self, height, chain=None):
-        print(height)
-        print(len(self.checkpoints))
         if constants.net.TESTNET:
             return 0, 0
         elif height // 2016 < len(self.checkpoints) and height % 2016 == 0:
