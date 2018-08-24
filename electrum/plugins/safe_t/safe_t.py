20c563c13d6d3a12d6d7cb4130bfec3dfe53ea4b
wakiyamap <wakiyamap@gmail.com>
2018-08-24 11:39:05 +0900

    fix safe_t Bitcoin->Monacoin

--- electrum/plugins/safe_t/safe_t.py
+++ electrum/plugins/safe_t/safe_t.py
@@ -153,7 +153,7 @@ class SafeTPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if constants.net.TESTNET else "Bitcoin"
+        return "Testnet" if constants.net.TESTNET else "Monacoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
