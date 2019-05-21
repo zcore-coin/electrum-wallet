4155c5fbacebcd36ea107b0eb13ee92ecb9763bc
y-chan <34832037+y-chan@users.noreply.github.com>
2019-05-21 13:41:24 +0900

    change default explorer (insight->blockbook)

--- electrum/util.py
+++ electrum/util.py
@@ -667,7 +667,7 @@ def block_explorer_info():
 
 def block_explorer(config: 'SimpleConfig') -> str:
     from . import constants
-    default_ = 'insight.bitzeny.jp'
+    default_ = 'zny.blockbook.ovh'
     be_key = config.get('block_explorer', default_)
     be = block_explorer_info().get(be_key)
     return be_key if be is not None else default_
