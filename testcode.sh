cc441e8a5988a5594213e37acdfa01441954d5ed
wakiyamap <wakiyamap@gmail.com>
2019-07-17 19:50:26 +0900

    Fix testcode more strictly

--- testcode.sh
+++ testcode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-grep -r "electrum\/" electrum_mona | grep -v "electrum\_mona\/locale"
-grep -r "import\ electrum " electrum_mona | grep -v "electrum\_mona\/locale"
-grep -r "import\ electrum\." electrum_mona | grep -v "electrum\_mona\/locale"
-grep -r "from\ electrum " electrum_mona | grep -v "electrum\_mona\/locale"
-grep -r "from\ electrum\." electrum_mona | grep -v "electrum\_mona\/locale"
-grep -r "electrum\.gui" electrum_mona | grep -v "electrum\_mona\/locale"
+grep -rI "electrum\/" ../electrum-mona . 2> /dev/null | grep -v -e "\/locale\/" -e "README" -e "RELEASE-NOTES" -e "electrum\/{version\.ELECTRUM\_VERSION}" -e "\/issues\/4994"
+grep -rI "import\ electrum " ../electrum-mona . 2> /dev/null | grep -v -e "\/locale\/" -e "README" -e "RELEASE-NOTES" -e "electrum\/{version\.ELECTRUM\_VERSION}" -e "\/issues\/4994"
+grep -rI "import\ electrum\." ../electrum-mona . 2> /dev/null | grep -v -e "\/locale\/" -e "README" -e "RELEASE-NOTES" -e "electrum\/{version\.ELECTRUM\_VERSION}" -e "\/issues\/4994"
+grep -rI "from\ electrum " ../electrum-mona . 2> /dev/null | grep -v -e "\/locale\/" -e "README" -e "RELEASE-NOTES" -e "electrum\/{version\.ELECTRUM\_VERSION}" -e "\/issues\/4994"
+grep -rI "from\ electrum\." ../electrum-mona . 2> /dev/null | grep -v -e "\/locale\/" -e "README" -e "RELEASE-NOTES" -e "electrum\/{version\.ELECTRUM\_VERSION}" -e "\/issues\/4994"
+grep -rI "electrum\.gui" ../electrum-mona . 2> /dev/null | grep -v -e "\/locale\/" -e "README" -e "RELEASE-NOTES" -e "electrum\/{version\.ELECTRUM\_VERSION}" -e "\/issues\/4994"
 pytest electrum_mona/tests/
